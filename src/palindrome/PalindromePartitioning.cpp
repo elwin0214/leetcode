69646ec2d18d2834132d1b117b7fb769ad778797
gwei <gongwei0214@gmail.com>
2015-11-04 09:30:30 +0800

    PalindromePartitioning & DistinctSubsequences

--- src/palindrome/PalindromePartitioning.cpp
+++ src/palindrome/PalindromePartitioning.cpp
@@ -16,4 +16,110 @@ Return
     ["a","a","b"]
   ]
 
-  */
+*/
+/*
+思路：
+字符串S=s1s2...sn 对字符串S可以分成的各种子串 进行判断求解。
+
+递归法，按照下面的过程可以处理所有的解。
+
+对于S[1,n+1],
+如果S[1,2]是回文，递归处理S[2,n+1]
+如果S[1,3]是回文，递归处理S[3,n+1]
+...
+如果S[1,n]是回文，递归处理S[n,n+1]
+
+
+也可以理解成一个树，dfs求解，类似于排列组合算法
+
+
+
+
+
+*/
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> partition(string s) 
+    {
+        vector<vector<string > > results;
+        vector<int> position;
+        partition(s, 0, position,results);
+        return results;
+    }
+
+    void partition(string &s, int pos, vector<int> &positions,vector<vector<string>> &results) 
+    {
+        if (pos >= s.size()) 
+        {
+            int start=0;
+            vector<string> result;
+            result.reserve(positions.size());
+            for (int i=0; i<positions.size(); i++) 
+            {
+                result.push_back(s.substr(start,positions[i]-start));
+                start = positions[i];
+            }
+            results.push_back(std::move(result));
+        }
+        for (int i=pos+1; i<=s.size(); i++) 
+        {
+            if (!isPalindrome(s, pos, i)) 
+            {
+                continue;
+            }
+            //cout<<i<<endl;
+            positions.push_back(i);
+            partition(s,i,positions,results);
+            positions.erase(positions.end()-1);
+        }
+    }
+
+    bool isPalindrome(string &s, int start,int end) 
+    {
+        end--;
+        while (start < end)
+        {
+            if(s[start++] == s[end--])
+                continue;
+            else 
+                return false;
+        }
+        return true;
+    }
+
+};
+
+void print(vector<vector<string>> results) 
+{
+    for(int i=0; i<results.size(); i++) 
+    {
+        for(int j=0; j<results[i].size(); j++) 
+        {
+            cout<<results[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+
+int main()
+{
+    Solution s;
+
+    vector<vector<string>> results= s.partition("abc");
+    print(results);
+
+    results= s.partition("aac");
+    print(results);
+
+    results= s.partition("aa");
+    print(results);
+    return 0;
+}
--- src/word/DistinctSubsequences.cpp
+++ src/word/DistinctSubsequences.cpp
@@ -15,6 +15,20 @@ S = "rabbbit", T = "rabbit"
 Return 3.
 
 */
+
+/*
+思路：
+S=s1s2...sn
+T=t1t2...tn
+
+分解成子问题，t1 与S 匹配情况，t1t2与S匹配情况.....
+
+  a b a b c
+a 1 0 1 0 0
+b 0 1 0 1 0   (在a的基础上)
+c 0 0 0 0 1   (在b的基础上)
+
+*/
 #include <iostream>
 #include <memory>
 
@@ -22,7 +36,94 @@ using namespace std;
 
 class Solution {
 public:
-  int numDistinct(string s, string t) {
-        
-  }
+    int numDistinct(string s, string t) 
+    {
+        int source_len=s.size();
+        int target_len=t.size();
+
+        int dp[target_len][source_len];
+        memset(dp, 0, sizeof(dp));
+
+        for (int i=0; i<target_len; i++)
+        {
+            bool hit = false;
+            for (int j=0; j<source_len; j++)
+            {
+                if (i > 0)
+                {
+                    if(!hit && (dp[i-1][j]==1))
+                    {
+                        hit=true;
+                        continue;
+                    }
+                }
+                else
+                {
+                    hit=true;
+                }
+            
+                if (hit)
+                {
+                //cout<<hit<<" "<<s[j] <<" "<<t[i]<<endl;
+                    if(t[i] == s[j])
+                    dp[i][j] = 1;
+                }
+            }
+            if (!hit) return 0;
+        }
+
+      
+        for (int i=0; i<target_len; i++)
+        {
+            int sum=0;
+            bool hit=false;
+            for (int j=0; j<source_len; j++)
+            {
+                if (!hit && dp[i][j]==1)
+                {
+                    hit=true;
+                }
+                if (!hit) continue;
+                if (0 == i )
+                {
+                    if(j < source_len - 1) dp[i][j+1] = dp[i][j+1]+dp[i][j];
+                }
+                else
+                {
+                    if (dp[i][j] < 0)
+                    {
+                        dp[i][j] = dp[i][j-1];
+                    }
+                    else
+                    {
+                        dp[i][j] = dp[i][j-1] + dp[i][j] * dp[i-1][j-1];
+                    }
+                }
+            }
+        }
+
+        // for (int i=0; i<target_len; i++)
+        // {
+        //     for(int j=0; j<source_len; j++)
+        //     {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[target_len-1][source_len-1];
+    }
+
+
 };
+
+
+int main()
+{
+    Solution sol;
+    cout<<sol.numDistinct("abc","ac")<<endl;
+    cout<<sol.numDistinct("abbcc","ac")<<endl;
+    cout<<sol.numDistinct("abbbcc","abc")<<endl;
+    cout<<sol.numDistinct("ddd","dd")<<endl;
+
+    return 0;
+}
