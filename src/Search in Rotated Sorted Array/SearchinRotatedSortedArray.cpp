420a9b4532c55f300746354f5bd4b5f0cac4329c
gwei <gongwei0214@gmail.com>
2015-11-24 13:55:17 +0800

    Search in Rotated Sorted Array

---
+++ src/Search in Rotated Sorted Array/SearchinRotatedSortedArray.cpp
@@ -0,0 +1,104 @@
+/*
+https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+Search in Rotated Sorted Array My Submissions Question
+Total Accepted: 79839 Total Submissions: 273687 Difficulty: Hard
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Subscribe to see which companies asked this question
+
+Show Tags
+Show Similar Problems
+Have you met this question in a real interview? Yes  No
+Discuss
+
+*/
+/*
+two pointers,不断把问题缩小。注意特殊情况，不要漏掉某些区间。
+
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution 
+{
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int start = 0;
+        int end = nums.size() - 1;
+
+        while (start <= end)
+        {
+
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[start] < nums[end])
+            {
+                if (nums[mid] < target)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            else //[start,end] 类似于 5 6 7 8 9 1 2 3 4
+            {
+                if (nums[start] <= nums[mid])  //step a [start,mid]递增 ，这里用<=把 最小的解，比如start==mid理解为递增数列处理, 
+                {
+                    if (nums[mid] < target || target < nums[start]) start = mid + 1;
+                    else end = mid - 1;
+                }
+                else //step b[mid,end]递增
+                {
+                    if (nums[mid] < target && target <= nums[end]) start = mid + 1;//必须是<=，不然会漏掉
+                    else end = mid - 1;
+                }
+               
+            }
+        }
+        return -1;   
+    }
+};
+
+int main()
+{
+    Solution sol;
+    {
+        vector<int> nums = { 6,7,8,9,2,3,4,5};
+        cout<<sol.search(nums,3)<<endl;
+        cout<<sol.search(nums,1)<<endl; 
+    }
+
+    {
+        vector<int> nums = {3,1};
+        cout<<sol.search(nums,1)<<endl; 
+        cout<<sol.search(nums,3)<<endl; 
+
+    }
+    {
+        vector<int> nums = {1,3};
+        cout<<sol.search(nums,1)<<endl; 
+        cout<<sol.search(nums,3)<<endl; 
+
+    }
+        {
+        vector<int> nums = {5,1,3};
+        cout<<sol.search(nums,1)<<endl; 
+        cout<<sol.search(nums,3)<<endl; 
+
+    }
+
+
+}
