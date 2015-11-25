246cd15244f5bc66d5b414c5dacf4fb4bd9ef852
gwei <gongwei0214@gmail.com>
2015-11-25 19:14:55 +0800

    question description

--- src/HIndex/HIndex.cpp
+++ src/HIndex/HIndex.cpp
@@ -1,3 +1,28 @@
+/*
+https://leetcode.com/problems/h-index/
+
+H-Index My Submissions Question
+Total Accepted: 17427 Total Submissions: 65105 Difficulty: Medium
+Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
+
+For example, given citations = [3, 0, 6, 1, 5], which means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively. Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, his h-index is 3.
+
+Note: If there are several possible values for h, the maximum one is taken as the h-index.
+
+Show Hint 
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+
+Subscribe to see which companies asked this question
+
+Show Tags
+Show Similar Problems
+
+
+
+*/
 #include <vector>
 #include <iostream>
 #include <string>
@@ -70,14 +95,18 @@ int main()
 	Solution s;
 
 	vector<int> v={1,4,2,5,9,2,4,6};
-	print(v);
 	cout<<s.hIndex(v)<<endl;
+	print(v);
 
 	v={3,0,6,1,5};
 	print(v);
 	cout<<s.hIndex(v)<<endl;
 
 	v={1,1,1,1,1};
+	print(v);
+	cout<<s.hIndex(v)<<endl;
+
+	v={3,100,100,100,100};
 	print(v);
 	cout<<s.hIndex(v)<<endl;
 	// s.quicksort(v, 0, v.size()-1);
