7f636d176b4d6129b04f4fb296fd4252c8a90760
gwei <gongwei0214@gmail.com>
2015-12-12 10:36:07 +0800

    clear

--- src/PopulatingNextRightPointersinEachNode/PopulatingNextRightPointersinEachNode.cpp
+++ src/PopulatingNextRightPointersinEachNode/PopulatingNextRightPointersinEachNode.cpp
@@ -42,25 +42,23 @@ using std::cout;
 using std::endl;
 using std::string;
 
-class Solution {
+class Solution 
+{
 public:
-	void connect(TreeLinkNode *root) 
-	{
-		if(NULL==root)
-			return;
-		TreeLinkNode *head=root;
-		head->next=NULL;
-		while(true)
-		{
-			if(NULL == head->left)
-			{
-				return;
-			}
+    void connect(TreeLinkNode *root) 
+    {
+        if (NULL == root) return;
+        TreeLinkNode *head=root;
+        head->next=NULL;
+        while (true)
+        {
+            if (NULL == head->left) return;
+		
 			TreeLinkNode *parent=head;
 			TreeLinkNode *prev=NULL;
-			while(true)
+            while (true)
 			{
-				parent->left->next=parent->right;
+                parent->left->next=parent->right;
 				prev=parent;
 				parent=parent->next;
 				if(NULL==parent)
